a47dc2b753c3eb331953688411debb184ee0e450
zhenght <zhenght@ct108.com>
2017-05-21 21:09:46 +0800

    jsontest

--- yueforapi/bottle_test.py
+++ yueforapi/bottle_test.py
@@ -1,5 +1,6 @@
 from bottle import route, run, template
 from datetime import datetime
+import json
 @route('/')
 def index(name='time'):
      dt = datetime.now()
@@ -7,5 +8,7 @@ def index(name='time'):
      return template('<b>Pi think the date/time is:{{t}}</b>', t = time)
 @route('/apitest/:input')
 def hello(input):
-        return 'hello world '+ input
+    json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
+    parse_json = json.load(json_string)
+    return 'hello world '+ input + parse_json["first_name"]
 run(host='192.168.0.116',port = 8090)
